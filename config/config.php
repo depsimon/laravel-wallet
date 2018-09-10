7703674f96cca1d8004b2ba978ddf7c93ef04b11
MannikJ <mannikj@web.de>
2018-09-10 14:17:32 +0200

    Comment config values

--- config/config.php
+++ config/config.php
@@ -1,14 +1,19 @@
 <?php
 
 return [
-/**
- * Change this if you extend the default Wallet Model
- */
+    /**
+     * Change this to specify the money amount column types
+     * If not explicitly set to 'decimal' integer columns are used
+     */
     'column_type' => env('WALLET_COLUMN_TYPE', 'decimal'),
+
+    /**
+     * Change this if you need to extend the default Wallet Model
+     */
     'wallet_model' => env('WALLET_MODEL', 'Depsimon\Wallet\Wallet'),
 
     /**
-     * Change this if you extend the default Transaction Model
+     * Change this if you need to extend the default Transaction Model
      */
     'transaction_model' => env('WALLET_TRANSACTION_MODEL', 'Depsimon\Wallet\Transaction'),
 ];
