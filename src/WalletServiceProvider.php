490c57fbc1676d1cbc63880749a40b4379db55e8
MannikJ <mannikj@web.de>
2018-09-13 14:48:57 +0200

    Change migrations and publishing

--- src/WalletServiceProvider.php
+++ src/WalletServiceProvider.php
@@ -16,9 +16,12 @@ class WalletServiceProvider extends ServiceProvider
                 __DIR__ . '/../config/config.php' => config_path('wallet.php'),
             ], 'config');
 
-            $this->publishes([
-                __DIR__ . '/../database/migrations/' => database_path('migrations'),
-            ], 'migrations');
+            if (!class_exists('CreateWalletTables')) {
+                $timestamp = date('Y_m_d_His', time());
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/2018_09_13_123456_create_wallet_tables.php' => database_path('migrations/' . $timestamp . '_create_wallet_tables.php'),
+                ], 'migrations');
+            }
         }
     }
 
