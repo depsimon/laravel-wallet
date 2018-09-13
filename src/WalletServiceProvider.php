d93f5967eb9a8adac260e5713897800856094d7f
MannikJ <mannikj@web.de>
2018-09-13 15:08:28 +0200

    Use loadMigration instead of publishing

--- src/WalletServiceProvider.php
+++ src/WalletServiceProvider.php
@@ -16,12 +16,7 @@ class WalletServiceProvider extends ServiceProvider
                 __DIR__ . '/../config/config.php' => config_path('wallet.php'),
             ], 'config');
 
-            if (!class_exists('CreateWalletTables')) {
-                $timestamp = date('Y_m_d_His', time());
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/2018_09_13_123456_create_wallet_tables.php' => database_path('migrations/' . $timestamp . '_create_wallet_tables.php'),
-                ], 'migrations');
-            }
+            $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         }
     }
 
